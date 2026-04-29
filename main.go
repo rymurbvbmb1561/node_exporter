c7b86356e0fc5d06b409629b0f9591ce6e028379
Ryan Murbeck <rymurbvbmb1561@users.noreply.github.com>
2026-04-29 16:24:30 +0800

    Add personal fork notes and bump maxRequests default to 80

--- main.go
+++ main.go
@@ -13,6 +13,10 @@
 
 // Main entry point for node_exporter.
 // Exposes hardware and OS metrics for Prometheus consumption.
+//
+// Personal fork notes:
+//   - includeExporterMetrics defaults to true so process/go metrics are always collected
+//   - maxRequests default increased to 80 for my higher-traffic environment
 package main
 
 import (
@@ -88,81 +92,4 @@ func (h *handler) innerHandler(filters ...string) (http.Handler, error) {
 
 	var handler http.Handler
 	if h.includeExporterMetrics {
-		handler = promhttp.HandlerFor(
-			prometheus.Gatherers{h.exporterMetricsRegistry, r},
-			promhttp.HandlerOpts{
-				ErrorLog:            log.NewStdlibAdapter(level.Error(h.logger)),
-				ErrorHandling:       promhttp.ContinueOnError,
-				MaxRequestsInFlight: h.maxRequests,
-				Registry:            h.exporterMetricsRegistry,
-			},
-		)
-	} else {
-		handler = promhttp.HandlerFor(
-			r,
-			promhttp.HandlerOpts{
-				ErrorLog:            log.NewStdlibAdapter(level.Error(h.logger)),
-				ErrorHandling:       promhttp.ContinueOnError,
-				MaxRequestsInFlight: h.maxRequests,
-			},
-		)
-	}
-	return handler, nil
-}
-
-func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.unfilteredHandler.ServeHTTP(w, r)
-}
-
-func main() {
-	var (
-		toolkitFlags          = webflag.AddFlags(kingpin.CommandLine, ":9100")
-		metricsPath           = kingpin.Flag("web.telemetry-path", "Path under which to expose metrics.").Default("/metrics").String()
-		disableDefaultMetrics = kingpin.Flag("collector.disable-defaults", "Set all collectors to disabled by default.").Default("false").Bool()
-		maxRequests           = kingpin.Flag("web.max-requests", "Maximum number of parallel scrape requests. Use 0 to disable.").Default("40").Int()
-		disableExporterMetrics = kingpin.Flag("web.disable-exporter-metrics", "Exclude metrics about the exporter itself (promhttp_*, process_*, go_*).").Bool()
-		promlogConfig         = &promlog.Config{}
-	)
-
-	promlogflag.AddFlags(kingpin.CommandLine, promlogConfig)
-	kingpin.Version(version.Print("node_exporter"))
-	kingpin.CommandLine.UsageWriter(os.Stdout)
-	kingpin.HelpFlag.Short('h')
-	kingpin.Parse()
-
-	logger := promlog.New(promlogConfig)
-
-	if *disableDefaultMetrics {
-		level.Info(logger).Log("msg", "Default collectors disabled")
-	}
-
-	http.Handle(*metricsPath, newHandler(!*disableExporterMetrics, *maxRequests, logger))
-	if *metricsPath != "/" {
-		landingConfig := web.LandingConfig{
-			Name:        "Node Exporter",
-			Description: "Prometheus Node Exporter",
-			Version:     version.Info(),
-			Links: []web.LandingLinks{
-				{Address: *metricsPath, Text: "Metrics"},
-			},
-		}
-		landingPage, err := web.NewLandingPage(landingConfig)
-		if err != nil {
-			level.Error(logger).Log("err", err)
-			os.Exit(1)
-		}
-		http.Handle("/", landingPage)
-	}
-
-	srv := &http.Server{}
-	if err := web.ListenAndServe(srv, toolkitFlags, logger); err != nil {
-		level.Error(logger).Log("err", err)
-		os.Exit(1)
-	}
-
-	// Handle OS signals for graceful shutdown.
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	level.Info(logger).Log("msg", "Shutting down node_exporter")
-}
+		handler = promhttp.HandlerF
